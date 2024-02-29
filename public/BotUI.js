361b556413d09cd5e695560555c9e9506860a5ae
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-02-29 21:29:06 +0100

    Added all schema types to test and formatted BotUI in public folder.

--- public/BotUI.js
+++ public/BotUI.js
@@ -3,135 +3,134 @@
  */
 
 var BotUi = function (id, structure, parent, attachTarget) {
-    this.id = id;
-    this.structure = structure;
-    this.parent = parent ? parent : null;
-    this.children = [];
-    this.attachTarget = attachTarget ? attachTarget : null;
-    this.element = null;
+  this.id = id;
+  this.structure = structure;
+  this.parent = parent ? parent : null;
+  this.children = [];
+  this.attachTarget = attachTarget ? attachTarget : null;
+  this.element = null;
 };
 
 BotUi.prototype.destroy = function () {
-    if(this.element.parentNode)
-        this.element.parentNode.removeChild(this.element);
+  if (this.element.parentNode)
+    this.element.parentNode.removeChild(this.element);
 };
 
 BotUi.prototype.create = function () {
-    var element = document.createElement("div");
-    element.className = "box";
-    var html = "";
-    for (var i in this.structure) {
-        var name = this.structure[i].name;
-        var label = this.structure[i].label;
-        var type = this.structure[i].type;
-        var options = this.structure[i].options;
-        switch (type) {
-            case "text":
-                if (!options)
-                    options = {
-                        value_foreground: "white"
-                    };
-                html += "<div class='" + name + " textDisplay boxRow'><div class='textDisplayLabel'>" + label + ": </div><div class='textDisplayValue' style='color: " + options.value_foreground + "; float:right'></div></div>";
-                break;
-            case "progressBar":
-                if (!options)
-                    options = {
-                        color: "green"
-                    };
-                html += "<div class='" + name + " progressBarDisplay boxRow'>  <div class='border'><div class='bar' style='background-color: " + options.color + "'> </div> <div class='barLabel'>" + label + ": <div class='value'>0%</div></div>  </div>  </div>";
-                break;
-                case "labelProgressBar":
-                    if (!options)
-                        options = {
-                            color: "green"
-                        };
-                    html += "<div class='" + name + " progressBarDisplay boxRow'>  <div class='border'><div class='bar' style='background-color: " + options.color + "'> </div> <div class='barLabel'>" + label + ": <div class='value'>0%</div></div>  </div>  </div>";
-                    break;
-            case "image":
-                if (!options) {
-                    options = {
-                        width: 200,
-                        height: 200
-                    };
-                }
-                html += "<div class='" + name + " imageDisplay boxRow'> <img src='' style='width:" + options.width + "px;height:" + options.height + "px;'/> </div>";
-                break;
-            case "graph":
-                //TODO implement later
-                break;
-            case "button":
-                if (!options) {
-                    options = {
-                        width: 200,
-                        height: 200
-                    };
-                }
-                html += "<div class='" + name + " imageDisplay boxRow'> <img src='' style='width:" + options.width + "px;height:" + options.height + "px;'/> </div>";
-                break;
-            case "botUI":
-                html += "<div class='" + name + " subBotUI boxRow'></div>";
-                break;
-
+  var element = document.createElement("div");
+  element.className = "box";
+  var html = "";
+  for (var i in this.structure) {
+    var name = this.structure[i].name;
+    var label = this.structure[i].label;
+    var type = this.structure[i].type;
+    var options = this.structure[i].options;
+    switch (type) {
+      case "text":
+        if (!options)
+          options = {
+            value_foreground: "white",
+          };
+        html += `<div class='${name} textDisplay boxRow'><div class='textDisplayLabel'>${label}: </div><div class='textDisplayValue' style='color: ${options.value_foreground}; float:right'></div></div>`;
+        break;
+      case "progressBar":
+        if (!options)
+          options = {
+            color: "green",
+          };
+        html += `<div class='${name} progressBarDisplay boxRow'>  <div class='border'><div class='bar' style='background-color: ${options.color}'> </div> <div class='barLabel'>${label}: <div class='value'>0%</div></div>  </div>  </div>`;
+        break;
+      case "labelProgressBar":
+        if (!options)
+          options = {
+            color: "green",
+          };
+        html += `<div class='${name} progressBarDisplay boxRow'>  <div class='border'><div class='bar' style='background-color: ${options.color}'> </div> <div class='barLabel'>${label}: <div class='value'>0%</div></div>  </div>  </div>`;
+        break;
+      case "image":
+        if (!options) {
+          options = {
+            width: 200,
+            height: 200,
+          };
         }
+        html += `<div class='${name} imageDisplay boxRow'> <img src='' style='width:${options.width}px;height:${options.height}px;'/> </div>`;
+        break;
+      case "graph":
+        //TODO implement later
+        break;
+      case "button":
+        if (!options) {
+          options = {
+            width: 200,
+            height: 200,
+          };
+        }
+        html += `<div class='${name} imageDisplay boxRow'> <img src='' style='width:${options.width}px;height:${options.height}px;'/> </div>`;
+        break;
+      case "botUI":
+        html += "<div class='" + name + " subBotUI boxRow'></div>";
+        break;
     }
-    element.innerHTML = html;
-    this.element = element;
-    if (this.parent) {
-        this.parent.children.push(this);
-        let container = this.parent.element.getElementsByClassName("subBotUI " + this.attachTarget)[0];
-        container.appendChild(element);
-    } else {
-        let container = document.getElementsByClassName("botUIContainer")[0];
-        container.appendChild(element);
-    }
+  }
+  element.innerHTML = html;
+  this.element = element;
+  if (this.parent) {
+    this.parent.children.push(this);
+    let container = this.parent.element.getElementsByClassName(
+      "subBotUI " + this.attachTarget
+    )[0];
+    container.appendChild(element);
+  } else {
+    let container = document.getElementsByClassName("botUIContainer")[0];
+    container.appendChild(element);
+  }
 };
 
 /**
  * Updates html object with data object
  */
 BotUi.prototype.render = function () {
-    if (!this.data)
-        return;
+  if (!this.data) return;
 
-    for (let i in this.structure) {
-        const name = this.structure[i].name;
-        const type = this.structure[i].type;
-        const value = this.data[name];
-        if (value === undefined)
-            continue;
-        const row = this.element.getElementsByClassName(name)[0];
-        switch (type) {
-            case "text":
-                row.getElementsByClassName("textDisplayValue")[0].innerHTML = value;
-                break;
-            case "progressBar":
-                row.getElementsByClassName("bar")[0].style.width = value + "%";
-                row.getElementsByClassName("value")[0].innerHTML = value + "%";
-                break;
-            case "labelProgressBar":
-                row.getElementsByClassName("bar")[0].style.width = value[0] + "%";
-                row.getElementsByClassName("value")[0].innerHTML = value[1];
-                break;
-            case "image":
-                row.getElementsByTagName("img")[0].src = value;
-                break;
-            case "graph":
-                //TODO implement later
-                break;
-            case "botUI":
-                break;
-        }
+  for (let i in this.structure) {
+    const name = this.structure[i].name;
+    const type = this.structure[i].type;
+    const value = this.data[name];
+    if (value === undefined) continue;
+    const row = this.element.getElementsByClassName(name)[0];
+    switch (type) {
+      case "text":
+        row.getElementsByClassName("textDisplayValue")[0].innerHTML = value;
+        break;
+      case "progressBar":
+        row.getElementsByClassName("bar")[0].style.width = value + "%";
+        row.getElementsByClassName("value")[0].innerHTML = value + "%";
+        break;
+      case "labelProgressBar":
+        row.getElementsByClassName("bar")[0].style.width = value[0] + "%";
+        row.getElementsByClassName("value")[0].innerHTML = value[1];
+        break;
+      case "image":
+        row.getElementsByTagName("img")[0].src = value;
+        break;
+      case "graph":
+        //TODO implement later
+        break;
+      case "botUI":
+        break;
     }
+  }
 };
 /**
  * Updates bot data
  */
 BotUi.prototype.update = function (data) {
-    this.data = data;
-    this.render();
+  this.data = data;
+  this.render();
 };
 
 BotUi.prototype.updateProperty = function (name, value) {
-    this.data[name] = value;
-    this.render();
+  this.data[name] = value;
+  this.render();
 };
--- test.js
+++ test.js
@@ -1,16 +1,31 @@
 /**
  * Created by Nexus on 15.08.2017.
+ * modified by thmsn
  */
 const BotWebInterface = require("./main");
 
-let BWI = new BotWebInterface({port: 2080});
+let BWI = new BotWebInterface({ port: 2080 });
 
 BWI.publisher.setDefaultStructure([
-    {name: "name", type: "text", label: "idasd"},
-    {name: "bots", type: "botUI", label: "Status"}
+  { name: "botUIId", type: "text", label: "BotUI ID" },
+  { name: "name", type: "text", label: "Name" },
+  { name: "ticks", type: "text", label: "ticks" },
+  {
+    name: "health",
+    type: "labelProgressBar", // this expects value to be a tuple the first value being the bar width and the second value being the text on the label
+    label: "Health",
+    options: { color: "red" },
+  },
+  {
+    name: "xp",
+    type: "progressBar", // this renders the value and sets the with of the bar to the value
+    label: "XP",
+    options: { color: "green" },
+  },
+  { name: "bots", type: "botUI", label: "Status" }, // botUI is a container used for "widgets" inside the "main" container
 ]);
 
-var i = 0;
+let setIntervalTicks = 0;
 /**
  *
  * @type {Array<BotUI>}
@@ -23,55 +38,93 @@ var subInterfaces = [];
  * @returns {BotUI}
  */
 function create() {
-    var a = BWI.publisher.createInterface();
-    a.setDataSource(function () {
-        return {
-            name: a.id,
-        }
-    });
+  const myArray = [
+    "apple",
+    "banana",
+    "cherry",
+    "date",
+    "elderberry",
+    "fig",
+    "grape",
+  ];
+  const randomElement = myArray[Math.floor(Math.random() * myArray.length)];
 
-    i++;
-    return a;
+  const botUI = BWI.publisher.createInterface();
+  botUI.setDataSource(function () {
+    return {
+      botUIId: botUI.id,
+      name: randomElement,
+      ticks: setIntervalTicks,
+    };
+  });
+
+  // TODO: how would we render a monster icon?
+  // TODO: what about an item icon?
+  let subBotUI1 = botUI.createSubBotUI(
+    [
+      { name: "foo", type: "text", label: "foo" },
+      { name: "id", type: "text", label: "id" },
+      { name: "namse", type: "text", label: "id" },
+      { name: "nsame", type: "text", label: "id" },
+    ],
+    "bots"
+  );
+  let subBotUI2 = botUI.createSubBotUI(
+    [
+      { name: "foo", type: "text", label: "foo" },
+      { name: "id", type: "text", label: "id" },
+      { name: "toggleActive", type: "button", label: "asd" },
+    ],
+    "bots"
+  );
+
+  return [botUI, subBotUI1, subBotUI2];
 }
 
 for (let l = 0; l < 4; l++) {
-    interfaces[l] = create();
+  interfaces[l] = create();
 }
+
 setInterval(function () {
-    var a = interfaces.shift();
-    a.destroy();
-    let botUI = create();
-    let subBotUI1 = botUI.createSubBotUI([
-        {name: "foo", type: "text", label: "foo"},
-        {name: "id", type: "text", label: "id"},
-        {name: "namse", type: "text", label: "id"},
-        {name: "nsame", type: "text", label: "id"},
-    ], "bots");
-    let subBotUI2 = botUI.createSubBotUI([
-        {name: "foo", type: "text", label: "foo"},
-        {name: "id", type: "text", label: "id"},
-        {name: "toggleActive", type: "button", label: "asd"}
-    ], "bots");
-    let i = 0;
-    a.setDataSource(function () {
-        return {
-            id: a.id,
-            foo: i++,
-        }
-    });
-    subBotUI1.setDataSource(function () {
-        return {
-            id: subBotUI1.id,
-            foo: i++,
-        }
-    });
-    subBotUI2.setDataSource(function () {
-        return {
-            id: subBotUI2.id,
-            foo: i++,
-        }
-    });
-    interfaces.push(botUI);
+  setIntervalTicks++;
+  let [botUI, subBotUI1, subBotUI2] = interfaces.shift();
+
+  // This destroy was probably for testing shutting down an interface and creating a new
+  if (setIntervalTicks % 100 == 0) {
+    botUI.destroy();
+    interfaces.push(create());
+    return;
+  }
+
+  let i = 0;
+  // Update data on the interfaces. this function will be evaluated often as Publisher will call it based on the updateRate
+  botUI.setDataSource(function () {
+    const maxHealth = 3000;
+    const health = Math.random() * maxHealth;
+    const healthPercentage = (100 * health) / maxHealth;
+
+    return {
+      botUIId: botUI.id,
+      ticks: setIntervalTicks,
+      // notice that if you refresh (F5) the BWI, the name is no longer present, this data source has overidden the previous one
+      health: [healthPercentage.toFixed(2), health.toFixed(2)],
+      xp: Math.random() * 100,
+    };
+  });
+
+  subBotUI1.setDataSource(function () {
+    return {
+      id: subBotUI1.id,
+      foo: i++,
+    };
+  });
+
+  subBotUI2.setDataSource(function () {
+    return {
+      id: subBotUI2.id,
+      foo: i++,
+    };
+  });
+
+  interfaces.push([botUI, subBotUI1, subBotUI2]);
 }, 1000);
-
-
